d7f372825fda3dc7c4b7a6582a8d38b832320f04
─꯭─⃝͎̽𓆩𝑫𝑬𝑽𝑰𝑳 𝑭𝑨𝑹'𝑯𝑨𝑵‣᭄𓆪___//😈🩵🪽 <uffxan576@gmail.com>
2026-03-10 01:08:48 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -6,7 +6,7 @@ let xfont = null;
 let yfont = null;
 let categoryEmoji = null;
 
-const HELP_GIF = "https://files.catbox.moe/4yp8k9.gif";
+const HELP_GIF = "https://i.postimg.cc/mrzwcJsh/GIF-20250712-114810-632.gif";
 
 /* ───── Load Fonts & Emoji ───── */
 async function loadResources() {
